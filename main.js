063a3f4f9f05ca8b189d993ccff9cea7abfc187c
Heloana12 <129884527+Heloana12@users.noreply.github.com>
2024-04-09 15:30:28 -0300

    s

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+
+for(let i=0;IDBObjectStore <botoes.length;i++{
+    botoes[i].onclick = function(){
+
+        for(let j=0;)
+        botoes[i].classList;add("ativo");
+    }
+    console.log(i);
+})
