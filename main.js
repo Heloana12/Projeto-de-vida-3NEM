3ea44b7ee7c70b774804c8cfa982fccb8b9e9957
Heloana12 <129884527+Heloana12@users.noreply.github.com>
2024-04-16 14:31:08 -0300

    s

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
+const text = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;IDBObjectStore <botoes.length;i++{
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remover("ativo");
+            textos[j].classList.remover("ativo");
         }
 
-        botoes[i].classList;add("ativo");
+        botoes[i].classList.add("ativo");
+        texto[i].classList.add("ativo");
     }
-})
+}
