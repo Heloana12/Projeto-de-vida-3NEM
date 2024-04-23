c5de25e675810a4ce22bc7d017f2f811522f13b3
Heloana12 <129884527+Heloana12@users.noreply.github.com>
2024-04-23 15:38:59 -0300

    s

--- main.js
+++ main.js
@@ -16,8 +16,16 @@ for(let i=0;IDBObjectStore <botoes.length;i++{
 
 const cotadore = document.querySelectorAll("contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
-contadores[0],textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+contadores[0],textContent = calculaTempo(tempo[0]);
+for (let i; i<contadores.length;i++){
+
+}
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
